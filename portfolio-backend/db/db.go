29fb97e4e109ae780b258bdcb5c5d99b7536c767
Pritesh Rathod <rathodpritesh0712@gmail.com>
2026-04-03 14:18:04 +0530

    migrate MongoDB from localhost to Atlas cluster

--- portfolio-backend/db/db.go
+++ portfolio-backend/db/db.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -14,12 +15,16 @@ var DB *mongo.Database
 var client *mongo.Client
 
 func ConnectDB() {
+	godotenv.Load()
+
+	mongoURI := os.Getenv("MONGO_URI")
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	var err error
 
-	client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
 
 	if err != nil {
 		log.Fatal(err)
@@ -27,7 +32,7 @@ func ConnectDB() {
 
 	DB = client.Database(os.Getenv("DB_NAME"))
 
-	log.Println("MongoDB connected")
+	log.Println("MongoDB Atlas Cluster Connected")
 }
 
 func GetClient() *mongo.Client {
