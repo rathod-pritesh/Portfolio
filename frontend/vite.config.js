85e7fdc787b9c02b50200b0d57d4529ece10bceb
Pritesh Rathod <rathodpritesh0712@gmail.com>
2026-05-28 15:22:58 +0530

    Fix: vite.config.js file import issue for API

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { API_BASE } from "$lib/config/api";
 
 export default defineConfig({
 	plugins: [
