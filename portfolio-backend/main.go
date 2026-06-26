036ee31eec9d144328ec4c64b647c0b754b9464c
Pritesh Rathod <rathodpritesh0712@gmail.com>
2026-06-26 18:07:47 +0530

    Domain name change for frontend

--- portfolio-backend/main.go
+++ portfolio-backend/main.go
@@ -17,7 +17,7 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins: []string{
 			"http://localhost:5173",
-			"https://portfolio-pied-five-58.vercel.app",
+			"https://priteshrathod.vercel.app",
 		},
 		AllowMethods: []string{
 			"GET", "POST", "PUT", "DELETE", "OPTIONS",
